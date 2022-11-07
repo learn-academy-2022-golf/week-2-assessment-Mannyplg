75e0008b82ac09e79ed9dd10bb7b03b9d3d799a8
Mannyplg <pettigrew.i@gmail.com>
2022-11-06 17:17:33 -0800

    wk-2-test-jest-completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,50 +10,150 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
+// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers 
+// and returns an array with all the numbers multiplied by 3.
 
+
+// Psuedo Code: Create a function called multiplyByThree
+        //      Parameters: 1 [array]
+        //      Iterate over array using .map method; mulltiply each value by three
+        //      array.map(value => value * 3 )
+        //      Multiply each value by 3
+        //      return: Mapped array
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
+//  describe("multiplyByThree", () => {
+//    it("returns an array with all the numbers multiplied by 3", () => {
+//          expect(multiplyByThree(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+//          expect(multiplyByThree(numbersArray2)).toEqual([72, 81, 90, 99, 108])
+//        })
+//  })
+
+ // b) Create the function that makes the test pass.
+// const multiplyByThree = (array) => {
+//    return array.map((value) => value * 3)
+//}
+
+// const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
+// const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
-// b) Create the function that makes the test pass.
+// Ran Test
+// ReferenceError: multiplyByThree is not defined
 
-// --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
+// Ran yarn jest--> Test Suites: 1 failed, 1 total
+//                  Tests:       1 failed, 1 total
 
+// Ran yarn jest--> Test Suites: 1 passed, 1 total
+//                  Tests:       1 passed, 1 total
+
+// --------------------1) Create a function that takes a object as an argument and 
+//                        decides if the number inside it is evenly divisible by three or not.
+
+// Prompt: Create a function which takes a object.--> const evedivThree(object). 
+// Inputs: Object
+// (Note: Instructor node...)-->emphasis...
+// Outputs: numbers inside it is easily divisible by three or not. 
+// 
 // a) Create a test with expect statements for each of the variables provided.
 
-const object1 = { number: 15 }
+// describe("evedivThree", () => {
+//   it("decides if the number inside it is evenly divisible by three or not.", () => {
+//     expect(evedivThree()).toEqual("15 is divisible by three")
+//    // expect(evedivThree(obj)).toEqual("15 is divisible by three")
+//   //  expect(evedivThree(obj)).toEqual("15 is divisible by three")
+//   })
+// })
+
+//     ReferenceError: evedivThree is not defined
+//     TypeError: Cannot read properties of undefined (reading 'values')
+// const evedivThree = (object) => object.keys.values.map((values) =>`${value} % 3 === 0) is divisible by three`)
+
+
+//const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
-const object2 = { number: 0 }
+//const object2 = { number: 0 }
 // Expected output: "0 is divisible by three"
-const object3 = { number: -7 }
+//const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
 // b) Create the function that makes the test pass.
 
-// --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
+// --------------------2) Create a function that takes in an array of words and returns an array with 
+//                        all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+// describe("capitlizer", () => {
+//   it("takes in an array of words and returns an array with all the words capitalized.", () => {
+//     expect(capitalizer(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+//     expect(capitalizer(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+//   })
+// })
+
+// Psuedocode
+// Prompt: Create a function that takes in an array of words.
+
+// Input: [array]
+// Output: Capitalize all words 
+// Iterate over array using .map(), to modify and return each value/word capitalized. 
+// Note: Used other methods(only slice the first value in index, then used a method to re
+//                          rest of string to be lowerCased).
+
+// ReferenceError: capitlizer is not defined
+
+// const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+// const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
 
 // b) Create the function that makes the test pass.
 
-// --------------------3) Create a function that takes in a string and logs the index of the first vowel.
+// const capitalizer = (array) => array.map((value) => value[0].toUpperCase() + value.slice(1).toLowerCase())
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
 
-// a) Create a test with expect statements for each of the variables provided.
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
 
-const vowelTester1 = "learn"
-// Expected output: 1
-const vowelTester2 = "academy"
-// Expected output: 0
-const vowelTester3 = "challenges"
-// Expected output: 2
+// // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
 
-// b) Create the function that makes the test pass.
+// Psuedocode
+// Prompt: Create a function that takes in a string and logs the index of the first vowel.
+
+// Input: String
+// Output: Logs the index of the first vowel
+// Iterate over array using .flter(), to returns index of the first vowel. 
+// Note: Used other methods(only slice the first value in index, then used a method to re
+//                          rest of string to be lowerCased).
+
+// // a) Create a test with expect statements for each of the variables provided.
+
+//     describe("logger", () => {
+//         it("takes in a string and logs the index of the first vowel", () => {
+//         expect(logger(vowelTester1)).toEqual(1)
+//         expect(logger(vowelTester2)).toEqual(0)
+//         expect(logger(vowelTester3)).toEqual(2)
+//      })
+// })
+
+// // ReferenceError: logger is not defined
+ 
+//  const vowelTester1 = "learn"
+// // // Expected output: 1
+
+// // Test Suites: 1 passed, 1 total
+// // Tests:       1 passed, 1 total
+
+//  const vowelTester2 = "academy"
+// // // Expected output: 0
+//  const vowelTester3 = "challenges"
+// // Expected output: 2
+
+// // Test Suites: 1 passed, 1 total
+// // Tests:       1 passed, 1 total
+
+// // b) Create the function that makes the test pass.
+
+//  const logger = (str) => str.search(/[a, e, i, o, u, A, E, I, O, U]/g) 
--- interview-questions.md
+++ interview-questions.md
@@ -8,37 +8,42 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-Your answer:
+Your answer: A parameter is a placeholder in a function. An argument translates that parameters meaning into a expression.
 
-Researched answer:
+Researched answer: function add(x, y) {
+                        return x + y
+                        }
+
+                    add(2, 3) --> 5.
+                    (e.g. x = 2, y = 3) x and y are parameters and 2 and 3 are the arguments.
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-Your answer:
+Your answer: .map predefined parameters: are function(), currentValue, index, array, and thisArg; index, array, and thisArg are optional.
 
-Researched answer:
+Researched answer: .map method creates a new array for each element from a array. 
 
 3. What is the difference between map and filter?
 
-Your answer:
+Your answer: map returns output in the same length of array. filter method only return targeted items in an array and returns those items.
 
-Researched answer:
+Researched answer: Research shows that one map method is used to convert values in an array. The filter method selects certain items of an array. 
 
 4. What is iteration?
 
-Your answer:
+Your answer: iteration is a process, which searches or seeks out target items to compute.
 
-Researched answer:
+Researched answer: Iteration is an sequence of instructions being repeated until result returns true.
 
 5. What is the difference between a class and an object?
 
-Your answer:
+Your answer: A class is the blueprint of what i want to create. An object follows that blueprint, however, variations of that class can be created.  
 
-Researched answer:
+Researched answer: Class is the blueprint that is used to create objects.
 
 6. STRETCH: What is hoisting in JavaScript?
 
-Your answer:
+Your answer: N/A
 
 Researched answer:
 
